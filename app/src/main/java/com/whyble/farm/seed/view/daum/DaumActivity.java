24c30445b2d671f9bf4132fb97e80ce9c30f92ab
silverlight2017@gmail.com <1213dlgudwns>
2019-01-08 10:19:46 +0900

    다음 zipcode개발중

---
+++ app/src/main/java/com/whyble/farm/seed/view/daum/DaumActivity.java
@@ -0,0 +1,15 @@
+package com.whyble.farm.seed.view.daum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.whyble.farm.seed.R;
+
+public class DaumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daum);
+    }
+}
