54f6204f2b81a7aa126e0c93097e513eb6627209
hyeongjoon <silverlight2017@gmail.com>
2019-01-13 23:45:17 +0900

    업데이트 부분 햄버거 버튼 추가

--- app/src/main/java/com/whyble/farm/seed/user/signup/update/UpdateActivity.java
+++ app/src/main/java/com/whyble/farm/seed/user/signup/update/UpdateActivity.java
@@ -10,32 +10,40 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.whyble.farm.seed.MainActivity;
 import com.whyble.farm.seed.R;
 import com.whyble.farm.seed.common.SharedPrefManager;
 import com.whyble.farm.seed.common.base.BaseActivity;
-import com.whyble.farm.seed.databinding.ActivitySignupBinding;
 import com.whyble.farm.seed.databinding.ActivityUpdateBinding;
-import com.whyble.farm.seed.domain.EditUser;
 import com.whyble.farm.seed.domain.ServerResponse;
 import com.whyble.farm.seed.domain.User;
-import com.whyble.farm.seed.user.signup.SignupActivity;
 import com.whyble.farm.seed.user.signup.login.LoginActivity;
 import com.whyble.farm.seed.util.TextManager.TextManager;
 import com.whyble.farm.seed.util.ValidationUtil;
 import com.whyble.farm.seed.util.device.DeviceUtils;
 import com.whyble.farm.seed.view.daum.DaumActivity;
+import com.whyble.farm.seed.view.seed.list.bonus.BonusSeedActivity;
+import com.whyble.farm.seed.view.seed.list.farm.FarmSeedActivity;
+import com.whyble.farm.seed.view.seed.list.my.MySeedActivity;
+import com.whyble.farm.seed.view.seed.list.save.SaveSeedActivity;
 
 import java.util.Calendar;
 
-public class UpdateActivity extends BaseActivity<UpdateActivity> implements UpdateIn.View {
+public class UpdateActivity extends BaseActivity<UpdateActivity> implements UpdateIn.View, NavigationView.OnNavigationItemSelectedListener {
 
     ActivityUpdateBinding binding;
 
@@ -60,6 +68,20 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
                 openCamera();
             }
         });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                getActivityClass(), drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -67,7 +89,7 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
         return UpdateActivity.this;
     }
 
-    public void onClickBackBtn(View view){
+    public void onClickBackBtn(View view) {
         finish();
     }
 
@@ -119,10 +141,8 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
 
     @Override
     public void textSignupResult(String s) {
-        Log.e("HJLEE", "s : " + s);
         Gson gson = new Gson();
         ServerResponse response = gson.fromJson(s, ServerResponse.class);
-        Log.e("HJLEE", "response : " + response.toString());
         if (response.getResult().matches("6")) {
             finish();
         } else {
@@ -141,11 +161,10 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
     }
 
     private void setPhoneumber(String userPhoneNumber) {
-        Log.e("HJLEE", "userPhoneNumber : " + userPhoneNumber);
-        if(userPhoneNumber != null){
-            binding.getDomain().setTel1(userPhoneNumber.substring(0,3));
-            binding.getDomain().setTel2(userPhoneNumber.substring(3,7));
-            binding.getDomain().setTel3(userPhoneNumber.substring(7,11));
+        if (userPhoneNumber != null) {
+            binding.getDomain().setTel1(userPhoneNumber.substring(0, 3));
+            binding.getDomain().setTel2(userPhoneNumber.substring(3, 7));
+            binding.getDomain().setTel3(userPhoneNumber.substring(7, 11));
         }
     }
 
@@ -204,7 +223,7 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
                             dialog.dismiss();
                         }
                     }).show();
-        }else{
+        } else {
             presenter.confirmRecommend(binding.getDomain().getRecommend());
         }
     }
@@ -240,7 +259,7 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
                             dialog.dismiss();
                         }
                     }).show();
-        }else if (ValidationUtil.isEmptyOfEditText(binding.id)) {
+        } else if (ValidationUtil.isEmptyOfEditText(binding.id)) {
             super.showBasicOneBtnPopup(null, "아이디를 입력하세요")
                     .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                         @Override
@@ -320,14 +339,6 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
                             dialog.dismiss();
                         }
                     }).show();
-     /*   } else if (ValidationUtil.isEmptyOfEditText(binding.address1)) {
-            super.showBasicOneBtnPopup(null, "상세주소를 입력하세요")
-                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    }).show();*/
         } else if (ValidationUtil.isEmptyOfEditText(binding.bankname)) {
             super.showBasicOneBtnPopup(null, "은행명을 입력하세요")
                     .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
@@ -348,7 +359,6 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
             binding.getDomain().setOld_pass(binding.oldPassword.getText().toString());
             binding.getDomain().setPasswd(binding.passwd.getText().toString());
             binding.getDomain().setPasswd2(binding.passwd2.getText().toString());
-            Log.e("HJLEE", binding.getDomain().toString());
             presenter.textLogin(binding.getDomain());
         }
     }
@@ -372,7 +382,7 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
         }
     }
 
-    public void onClickBirthday(View view){
+    public void onClickBirthday(View view) {
         final Calendar cldr = Calendar.getInstance();
         int day = cldr.get(Calendar.DAY_OF_MONTH);
         int month = cldr.get(Calendar.MONTH);
@@ -384,9 +394,39 @@ public class UpdateActivity extends BaseActivity<UpdateActivity> implements Upda
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         binding.getDomain().setBirthday(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
                         binding.birthday.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
-//                        binding.getDate.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
                     }
                 }, year, month, day);
         picker.show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if(id == R.id.draw_save_seed){
+            Intent intent = new Intent(getApplicationContext(), SaveSeedActivity.class);
+            startActivity(intent);
+            finish();
+        }else if(id == R.id.draw_farm_seed){
+            Intent intent = new Intent(getApplicationContext(), FarmSeedActivity.class);
+            startActivity(intent);
+            finish();
+        }else if(id == R.id.draw_harvest_history){
+            Intent intent = new Intent(getApplicationContext(), MySeedActivity.class);
+            startActivity(intent);
+            finish();
+        }else if(id == R.id.draw_bonus_seed){
+            Intent intent = new Intent(getApplicationContext(), BonusSeedActivity.class);
+            startActivity(intent);
+            finish();
+        }else if(id == R.id.logout){
+            mSharedPrefManager.removeAllPreferences();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            ((MainActivity)MainActivity.mContext).finish();
+            finish();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
